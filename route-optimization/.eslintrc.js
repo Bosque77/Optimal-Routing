986afc1ae1dfc4e0d54e6d981329b36098fe5c8e
forest <f.schwartz@localeventsupplies.com>
2021-12-07 18:24:52 -0800

    started setting up the form

--- route-optimization/.eslintrc.js
+++ route-optimization/.eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
     'env': {
         'browser': true,
         'commonjs': true,
-        'es2021': true
+        'es2021': true,
+        'jest': true
     },
     'extends': [
         'eslint:recommended',
+        'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended'
     ],
     'parser': '@typescript-eslint/parser',
@@ -15,7 +17,15 @@ module.exports = {
     'plugins': [
         '@typescript-eslint'
     ],
+    'settings': {
+        'react': {
+            'pragma': 'React',
+            'version': 'detect'
+        }
+    },
     'rules': {
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0,
         'indent': [
             'error',
             4
--- route-optimization/src/components/SignInForm/SignInForm.tsx
+++ route-optimization/src/components/SignInForm/SignInForm.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SignInForm.css'
 import logo from './images/logo.svg'
 import {
     Link
 } from 'react-router-dom'
 
+
+
 const SignInForm = () => {
+
+    const [email, setEmail] = useState<string>('')
+
+
+    // const onEmailChange = (target : React.FormEvent<HTMLInputElement>) =>{
+    //     console.log(target.currentTarget.value)
+    //     setEmail(target.currentTarget.value)
+    // }
+
+    const handleLogin = (event: React.SyntheticEvent) => {
+        event.preventDefault()
+        console.log('inside form login')
+    }
+
+
     return (
         <div className="sign-up-wrap"><img src={logo} alt="" />
             <div className="w-form">
                 <div className="placeholder-text-style-embed-code w-embed">
                 </div>
-                <form id="email-form" name="email-form" data-name="Email Form" className="form" autoComplete="off"><input type="email" className="text_field" maxLength={256} name="email" data-name="Email"
-                    placeholder="Email Address" id="email" autoCapitalize="off"/><input type="password" className="text_field"
+                <form id="email-form" name="email-form" data-name="Email Form" className="form" autoComplete="off" onSubmit={handleLogin}><input type="email" className="text_field" maxLength={256} name="email" data-name="Email"
+                    placeholder="Email Address" id="email" autoCapitalize="off" value={email} onChange={({target})=>setEmail(target.value)}/><input type="password" className="text_field"
                     maxLength={256} name="Password" data-name="Password" placeholder="Password" id="Password" /><input
                     type="submit" value="Sign In" data-wait="Please wait..." className="btn grey darken-1"/></form>
                 <div className="w-form-done">
