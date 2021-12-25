7c9bcad76742e44d923df5cd8d42580b94e510d4
forest <f.schwartz@localeventsupplies.com>
2021-12-25 08:39:33 -0800

    got initDB.ts working correctly

--- server/initDB.ts
+++ server/initDB.ts
@@ -2,20 +2,39 @@ import mongoose from 'mongoose'
 import config from './utils/config'
 import logger from './utils/logger'
 import { User } from './models/user'
+// import bcrypt from 'bcrypt'
 
 
-const submitUser = async() =>{
-    let new_user = new User({username: 'forest', password:'test123'})
-
-    let returned_user = await new_user.save()
-    mongoose.disconnect()
-    
-    console.log(returned_user)
-  }
 
 
-if (config.MONGODB_URI) {
-    mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+const users = [
+  {
+    "username": "user_1",
+    "password": "password123",
+    "passwordHash": "$2b$10$drQ3m/uP5sejRRlfHNwNfu4Esxec454XqrE9j6ivWG8E/pYa4HF1u",
+    "id": "61c7483607e4533869b9ec08"
+  },
+  {
+    "username": "user_2",
+    "password": "password1234",
+    "passwordHash": "$2b$10$//cykx4aefD9n4YOKYB7kuFGGoHto3izcLWxvmgyZSc0GnAIf8Oh6",
+    "id": "61c7483607e4533869b9ec09"
+  }]
+
+
+// const saltRounds = 10
+
+
+// const createPasswordHash = async(password:string, saltRounds: number) => {
+//     const password_hash = await bcrypt.hash(password, saltRounds)
+//     return password_hash
+// }
+
+
+
+const initUsers = async() =>{
+  if (config.MONGODB_URI) {
+    await mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
       .then(() => {
         logger.info('connected to MongoDB')
       })
@@ -24,6 +43,20 @@ if (config.MONGODB_URI) {
       })
   }
 
+    await User.deleteMany({})
 
-submitUser()
+    const user_objects = users.map(user => new User({_id:user.id, username: user.username, passwordHash: user.passwordHash}) )
+    const promise_array = user_objects.map(user => user.save())
+    await Promise.all(promise_array)
+
+
+    mongoose.disconnect()
+
+  }
+
+
+
+
+
+initUsers()
 
--- server/types.ts
+++ server/types.ts
@@ -1,4 +1,4 @@
 export interface NewUser {
     username: string,
-    password: string
+    password: string,
 }
