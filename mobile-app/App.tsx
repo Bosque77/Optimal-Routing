5d2ccc442b3db8944ee9520060bf5f85d50af728
Forest <forestschwrtz@gmail.com>
2022-06-07 18:13:30 -0700

    configuring routing

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SignIn from './src/components/SignIn';
-import { NativeBaseProvider } from "native-base";
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import SignIn from './src/components/SignIn'
+import { NativeBaseProvider } from "native-base"
 import { store } from './src/state/store'
 import { Provider } from 'react-redux'
+import Main from './src/components/Main'
+import { NavigationContainer } from '@react-navigation/native';
+import { NativeRouter } from 'react-router-native';
+
+
 
 export default function App() {
+
   return (
-    <Provider store={store} >
-      <NativeBaseProvider >
-        <View style={styles.container}>
-          <SignIn />
-          <StatusBar style="auto" />
-        </View>
-      </NativeBaseProvider>
-    </Provider>
+    <>
+
+      <NativeRouter>
+        <Provider store={store} >
+          <NativeBaseProvider >
+            <View style={styles.container}>
+              <SignIn />
+              <StatusBar style="auto" />
+            </View>
+          </NativeBaseProvider>
+        </Provider>
+      </NativeRouter>
+
+    </>
 
 
   );
---
+++ mobile-app/src/components/Main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text } from 'native-base'
+// import {
+//     Router, Route, Routes
+// } from 'react-router-native'
+
+
+const Main = () => {
+    return (
+        <>
+            <Text>Test</Text>
+            {/* <Routes>
+                <Route path="/" element={<RepositoryList />} exact />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes> */}
+        </>
+
+    )
+}
+
+export default Main
