8e21b9f1547138ebbaca0097411ca9c758547a4f
Forest <forestschwrtz@gmail.com>
2022-08-27 18:16:42 -0700

    finished the lat_lng_list generator and storing info in json

--- lambda/GenerateRandomOrders/lambda_function.py
+++ lambda/GenerateRandomOrders/lambda_function.py
@@ -1,12 +1,114 @@
-import json, requests
+import json
+import requests
+from dotenv import load_dotenv   # Delete this on the production build
+
+import boto3
+import base64
+from botocore.exceptions import ClientError
+
+# STATUS
+DEBUG = True
+
+# url variable store url
+base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+
+
+def generate_zipcodes():
+    zipcodes = []
+    file_name = 'zipcodes.txt'
+    with open(file_name) as in_file:
+        for line in in_file:
+            data = in_file.readline()
+
+            if len(data) != 0:
+                zipcodes.append(int(data))
+
+    
+    return zipcodes
+
+
+def get_secret():
+
+    secret_name = "Google_API_Key"
+    region_name = "us-west-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS key.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+            return secret
+        else:
+            decoded_binary_secret = base64.b64decode(
+                get_secret_value_response['SecretBinary'])
+
+    # Your code goes here.
+
 
 def lambda_handler(event, context):
 
-    print(event)
-    print(context)
+    if DEBUG != TRUE:
+
+        api_key = get_secret()
+    else:
+        load_dotenv()
+        api_key = GOOGLE_API_KEY
 
     # TODO implement
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
+if __name__ == "__main__":
+    event = {
+        "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
+        "detail-type": "Scheduled Event",
+        "source": "aws.events",
+        "account": "123456789012",
+        "time": "1970-01-01T00:00:00Z",
+        "region": "{region}",
+        "resources": [
+            "arn:{partition}:events:{region}:123456789012:rule/my-schedule"
+        ],
+        "detail": {}
+    }
+
+    lambda_handler(event, None)
--- lambda/GenerateRandomOrders/main.py
+++ lambda/GenerateRandomOrders/main.py
@@ -9,14 +9,19 @@
 # query using Google Places API
 
 # importing required modules
-import requests, json
+import requests, json, random
+from dotenv import dotenv_values
 
-# enter your api key here
-api_key = 'AIzaSyDBWA8Gu8uc_uOL6Sp2ZIFsRI53PKbAjkw'
+# STATUS
+DEBUG = True
+
 # url variable store url
 base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
-# a generator that provides zipcodes
 
+
+
+def get_secret():
+    pass
 
 def generate_zipcodes():
     zipcodes = []
@@ -28,34 +33,81 @@ def generate_zipcodes():
             if len(data) != 0:
                 zipcodes.append(int(data))
 
-    zip_generator = (zipcode for zipcode in zipcodes)
-    return zip_generator
+    return zipcodes
 
 
-
-
-def generate_places():
+def generate_random_lat_lng_list(num_of_coordinates):
     """
-    I will get a list of lat, lngs in georgia and then determine addresses
-    :return:
+    creates a list of coordinates equal to the length num_of_coordinates
+
+    Parameters
+    ----------
+    num_of_coordinates
+
+    Returns
+    -------
+    lat_lng_list  :  this is a list of tuples that contain a latitude and longitude
     """
+    lat_lng_list = []
+
+
+    if DEBUG != True:
+        api_key = get_secret()
+    else:
+        config = dotenv_values(".env")
+        api_key = config["GOOGLE_API_KEY"]
     zipcodes = generate_zipcodes()
+    num_of_zipcodes = len(zipcodes)
 
-    zipcode = next(zipcodes)
-    print(zipcode)
-    query = f"'businesses in {zipcode} '"
 
-    url = f'{base_url}?query={query}&key={api_key}'
 
-    print(url)
+    while len(lat_lng_list) < num_of_coordinates:
 
-    response = requests.get(url)
-    response_as_json = response.json()
-    results = response_as_json['results']
-    lat = results['geometry']['location']['lat']
-    lng = results['geometry']['location']['lat']
-    print(json.dumps(response_as_json, indent=4, sort_keys=True))
+        # getting a random zipcode from the zipcodes list
+        rand_num = random.randrange(0,num_of_zipcodes)
+        zipcode = zipcodes[rand_num]
 
+
+        # looking up businesses in that region
+        query = f"'businesses in {zipcode} '"
+        url = f'{base_url}?query={query}&key={api_key}'
+        response = requests.get(url)
+        response_as_json = response.json()
+        results = response_as_json['results']
+
+
+
+        # selecting an arbitrary number of businesses from that region
+        num_of_results = len(results)
+        num_of_orders_from_this_zipcode = num_of_results // 5
+
+        if num_of_orders_from_this_zipcode > 0:
+            for i in range(num_of_orders_from_this_zipcode):
+                random_result = random.randrange(0, num_of_results)
+                business = results[random_result]
+
+                lat = business['geometry']['location']['lat']
+                lng = business['geometry']['location']['lat']
+
+                lat_lng = (lat, lng)
+                lat_lng_list.append(lat_lng)
+        else:
+            random_result = random.randrange(0, num_of_results)
+            business = results[random_result]
+
+            lat = business['geometry']['location']['lat']
+            lng = business['geometry']['location']['lng']
+
+            lat_lng = (lat, lng)
+            lat_lng_list.append(lat_lng)
+
+
+    # this is temporary so i dont over use my google requests
+    with open('lat_lng_list.json', 'w') as outfile:  # just 'w' since it's a text file
+        json.dump(lat_lng_list, outfile)
+
+    return lat_lng_list
 
 if __name__ == "__main__":
-    generate_places()
+    lat_lng_list = generate_random_lat_lng_list(10)
+    print('pause here')
