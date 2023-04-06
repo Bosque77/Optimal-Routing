65384603c70186df28dcb133aae3f9180180e9aa
Forest <forestschwrtz@gmail.com>
2023-04-05 20:37:38 -0700

    added the google map component

---
+++ client/src/components/GoogleMapWithMarkers.tsx
@@ -0,0 +1,41 @@
+// src/components/GoogleMapWithMarkers.tsx
+
+import React, { useEffect, useRef } from 'react';
+
+interface GoogleMapWithMarkersProps {
+  centerLatitude: number;
+  centerLongitude: number;
+  markers: Array<{ lat: number; lng: number }>;
+}
+
+const GoogleMapWithMarkers: React.FC<GoogleMapWithMarkersProps> = ({
+  centerLatitude,
+  centerLongitude,
+  markers,
+}) => {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const initMap = () => {
+      if (!mapRef.current || typeof google === 'undefined') return;
+
+      const map = new google.maps.Map(mapRef.current, {
+        center: { lat: centerLatitude, lng: centerLongitude },
+        zoom: 12,
+      });
+
+      markers.forEach((marker) => {
+        new google.maps.Marker({
+          position: { lat: marker.lat, lng: marker.lng },
+          map,
+        });
+      });
+    };
+
+    initMap();
+  }, [centerLatitude, centerLongitude, markers]);
+
+  return <div ref={mapRef} style={{ height: '100%', width: '100%' }} />;
+};
+
+export default GoogleMapWithMarkers;
--- client/src/pages/RoutePage/RoutePage.tsx
+++ client/src/pages/RoutePage/RoutePage.tsx
@@ -1,19 +1,29 @@
 import Alert from "../../components/Alert";
 import SideNav from "../../components/SideNav/SideNav";
-
+import GoogleMapWithMarkers from "../../components/GoogleMapWithMarkers";
 
 const RoutePage = () => {
-    return (
-        <div className="bg-slate-100 flex h-full">
-          <Alert />
-          <div className="w-64 bg-slate-50">
-            <SideNav />
-          </div>
+  const markers = [
+    { lat: 40.7128, lng: -74.006 },
+    { lat: 40.73061, lng: -73.935242 },
+    { lat: 40.706086, lng: -74.009051 },
+  ];
 
-      
-        </div>
-      );
-
-}
+  return (
+    <div className="bg-slate-100 flex h-full">
+      <Alert />
+      <div className="w-64 bg-slate-50">
+        <SideNav />
+      </div>
+      <div style={{ height: "500px", width: "100%" }}>
+        <GoogleMapWithMarkers
+          centerLatitude={40.7128}
+          centerLongitude={-74.006}
+          markers={markers}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default RoutePage;
