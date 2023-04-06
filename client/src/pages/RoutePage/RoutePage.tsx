360d0477544c680e814d431f39026129810069e0
Forest <forestschwrtz@gmail.com>
2023-04-05 20:59:18 -0700

    added date selector to map page

--- client/src/pages/RoutePage/RoutePage.tsx
+++ client/src/pages/RoutePage/RoutePage.tsx
@@ -1,6 +1,11 @@
 import Alert from "../../components/Alert";
 import SideNav from "../../components/SideNav/SideNav";
 import GoogleMapWithMarkers from "../../components/GoogleMapWithMarkers";
+import RegionSelector from "../../components/RegionSelector";
+
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 const RoutePage = () => {
   const markers = [
@@ -9,18 +14,38 @@ const RoutePage = () => {
     { lat: 40.706086, lng: -74.009051 },
   ];
 
+  const [selectedDate, handleDateChange] = useState(new Date());
+
   return (
-    <div className="bg-slate-100 flex h-full">
+    <div className="bg-slate-100 flex h-screen">
       <Alert />
       <div className="w-64 bg-slate-50">
         <SideNav />
       </div>
-      <div style={{ height: "500px", width: "100%" }}>
-        <GoogleMapWithMarkers
-          centerLatitude={40.7128}
-          centerLongitude={-74.006}
-          markers={markers}
-        />
+      <div className="mx-auto flex flex-col w-2/3">
+        <div className="flex flex-row justify-end">
+          <RegionSelector />
+        </div>
+
+        <div
+          className="mx-auto mt-8"
+          style={{ height: "500px", width: "100%" }}
+        >
+          <GoogleMapWithMarkers
+            centerLatitude={40.7128}
+            centerLongitude={-74.006}
+            markers={markers}
+          />
+          <div className="mt-6 text-left">
+              <label className="ml-2" >Select Date</label>
+              <DatePicker
+                selected={selectedDate}
+                onChange={(date: Date) => handleDateChange(date)}
+                className="border-2 rounded w-48 p-2"
+              />
+
+          </div>
+        </div>
       </div>
     </div>
   );
