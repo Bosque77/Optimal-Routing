ca3b07bd5f3e16d7b069125592601bbdb9c5b971
Forest <forestschwrtz@gmail.com>
2023-04-28 07:21:35 -0700

    lambda may be working

--- lambda/RouteOptimizer/test_lambda_function.py
+++ lambda/RouteOptimizer/test_lambda_function.py
@@ -5,10 +5,14 @@ import lambda_function
 
 def testCreateRoutes():
     # Testing CreateRoutes Lambda Function
-    test_file_location = './json_objects/createRoutes_Event.json'
-    f = open(test_file_location)
-    event = json.load(f)
-    response = lambda_function.create_routes(event)
+    with open('createRoutes_Data.json', 'r') as file:
+        json_data = json.load(file)
+
+    event = {
+    "body": json.dumps(json_data)
+    }
+    dummy_context = None
+    response = lambda_function.create_routes(event, dummy_context)
     print(response)
 
 
