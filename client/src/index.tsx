1a761b9460cd83df949501d071f598809401b461
Forest <forestschwrtz@gmail.com>
2023-06-11 16:24:49 -0700

    UPDATED REACTION DOM RENDER TO USE REACT V18 METHODS.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,15 +4,16 @@ import './index.css'
 import App from './App'
 import { store } from './state/store'
 import { Provider } from 'react-redux'
+import {createRoot} from  'react-dom/client'
+
+const root = createRoot(document.getElementById('root')!);
 
 
-ReactDOM.render(
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
     </React.StrictMode>,
-    document.getElementById('root')
-)
-
+  );
 
