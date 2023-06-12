87e40fcde8b96617c72e9a82d4a01a91eef9b3f2
Forest <forestschwrtz@gmail.com>
2023-06-11 18:09:37 -0700

    Add card element is looking pretty good.

--- client/src/pages/AccountPage/AddCardPayment.tsx
+++ client/src/pages/AccountPage/AddCardPayment.tsx
@@ -1,12 +1,20 @@
-import React, { FC, FormEvent } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-
-
+import React, { FC, FormEvent, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 
 const AddCardForm: FC = () => {
   const stripe = useStripe();
   const elements = useElements();
+
+  // Set up state for the form input
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [zipcode, setZipcode] = useState("");
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -15,12 +23,21 @@ const AddCardForm: FC = () => {
       return;
     }
 
-    const card = elements.getElement(CardElement);
+    const cardElement = elements.getElement(CardElement);
 
-    if (card) {
+    const cardHolderName = name;
+    const cardHolderAddress = address;
+
+    if (cardElement) {
       const result = await stripe.createPaymentMethod({
-        type: 'card',
-        card: card,
+        type: "card",
+        card: cardElement,
+        billing_details: {
+          name: cardHolderName,
+          address: {
+            line1: cardHolderAddress,
+          },
+        },
       });
 
       if (result.error) {
@@ -32,9 +49,43 @@ const AddCardForm: FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement />
-      <button type="submit" disabled={!stripe}>
+    <form onSubmit={handleSubmit} className="flex flex-col px-6 py-4">
+      <label className="font-medium text-gray-700 my-1">Name on Card</label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="border rounded-md border-gray-300 pl-2 py-2 mb-4 focus:outline-none focus:border-indigo-500 focus:border-2 sm:text-sm text-left w-64"
+        placeholder="Name on Card"
+      />
+
+      <label className="font-medium text-gray-700 my-1">Billing Address</label>
+      <input
+        type="text"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+        className="border rounded-md border-gray-300 pl-2 py-2 mb-6 focus:outline-none focus:border-indigo-500 focus:border-2 sm:text-sm text-left w-64"
+        placeholder="Address"
+      />
+
+      <label className="font-medium text-gray-700 my-1">Zipcode</label>
+      <input
+        type="text"
+        value={zipcode}
+        onChange={(e) => setZipcode(e.target.value)}
+        className="border rounded-md border-gray-300 pl-2 py-2 mb-6 focus:outline-none focus:border-indigo-500 focus:border-2 sm:text-sm text-left w-64"
+        placeholder="Zipcode"
+      />
+
+      <div className="mb-6 border rounded px-4 py-2">
+        <CardElement />
+      </div>
+
+      <button
+        type="submit"
+        disabled={!stripe}
+        className="mt-4 bg-indigo-600 text-white rounded-md px-4 py-2 hover:bg-indigo-700"
+      >
         Add Card
       </button>
     </form>
@@ -42,11 +93,11 @@ const AddCardForm: FC = () => {
 };
 
 export const AddCardPayment: FC = () => {
-
-// load Stripe.js with your publishable key.
-  console.log('logging the stripe api key')
-  console.log(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY_TEST)
-  const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY_TEST!);
+  console.log("logging the stripe api key");
+  console.log(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY_TEST);
+  const stripePromise = loadStripe(
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY_TEST!
+  );
 
   return (
     <Elements stripe={stripePromise}>
